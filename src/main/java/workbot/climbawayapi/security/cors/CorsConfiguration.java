93f49d3b4e5d0ef916af5d9341b4946a279af570
Miguel Alfaro <miguelalfaro2017@gmail.com>
2022-11-21 20:46:09 -0500

    refactor: cors

--- src/main/java/workbot/climbawayapi/security/cors/CorsConfiguration.java
+++ src/main/java/workbot/climbawayapi/security/cors/CorsConfiguration.java
@@ -2,20 +2,20 @@ package workbot.climbawayapi.security.cors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+;
 
 @Configuration
-public class CorsConfiguration  {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedOrigins("*")
-                        .allowedHeaders("*");
-            }
-        };
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(-1);
     }
 }
