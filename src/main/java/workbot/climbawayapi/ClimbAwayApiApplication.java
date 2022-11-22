67b0761b25afb4a8d07e6f1d81e1b48a4a56e7f2
Miguel Alfaro <miguelalfaro2017@gmail.com>
2022-11-21 20:30:12 -0500

    refactor: cors

--- src/main/java/workbot/climbawayapi/ClimbAwayApiApplication.java
+++ src/main/java/workbot/climbawayapi/ClimbAwayApiApplication.java
@@ -3,7 +3,10 @@ package workbot.climbawayapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
 @SpringBootApplication
 @EnableJpaAuditing
 public class ClimbAwayApiApplication {
